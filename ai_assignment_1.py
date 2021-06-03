10421b98c3d6aee84b8e6d2130164a4189a21768
Adrian <101zoomzoom@gmail.com>
2021-06-03 15:42:08 -0400

    first commit

---
+++ ai_assignment_1.py
@@ -0,0 +1,38 @@
+city_paths={
+    'Arad':           [ ['',71] , ['',151] ],
+    'Bucharest':      [ ['',71] , ['',151] ],
+    'Craiova':        [ ['',71] , ['',151] ],
+    'Dobreta':        [ ['',71] , ['',151] ],
+    'Eforie':         [ ['',71] , ['',151] ],
+    'Fagaras':        [ ['',71] , ['',151] ],
+    'Giurgiu':        [ ['',71] , ['',151] ],
+    'Hirsova':        [ ['',71] , ['',151] ],
+    'Iasi':           [ ['',71] , ['',151] ],
+    'Lugoj':          [ ['',71] , ['',151] ],
+    'Mehadia':        [ ['',71] , ['',151] ],
+    'Neamt':          [ ['',71] , ['',151] ],
+    'Oradea':         [ ['Zerind',71] , ['Sibiu',151] ],
+    'Pitesti':        [ ['',71] , ['',151] ],
+    'Rimnicu Vilcea': [ ['',71] , ['',151] ],
+    'Sibiu':          [ ['',71] , ['',151] ],
+    'Timisoara':      [ ['',71] , ['',151] ],
+    'Urziceni':       [ ['',71] , ['',151] ],
+    'Vaslui':         [ ['',71] , ['',151] ],
+    'Zerind':         [ ['',71] , ['',151] ],
+}
+
+def bfs(root_city,distance_traveled,city_history):
+  return 0
+
+def find_shortest_route():
+  print("Purpose of app here")
+  print("Which city do you want to depart from?")
+  #print list of cities, could be list of keys from city_paths
+  #accept city input
+  print("Which algorithm would you like to use?")
+  #print algos
+  #accept algo input
+  #select algorithm function
+  #return(algo[root_city,0,[]])
+  return 0
+find_shortest_route()
