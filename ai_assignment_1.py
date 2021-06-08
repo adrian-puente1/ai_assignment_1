055364e1a5c48f3e1bb84f8c4a332e677458c2cb
Adrian <101zoomzoom@gmail.com>
2021-06-08 16:59:48 -0400

    fixed city_paths, mostly functional astar

--- ai_assignment_1.py
+++ ai_assignment_1.py
@@ -1,9 +1,8 @@
 from os import path
 
 DESTINATION='Bucharest'
-
 city_paths = {
-    'Arad':             [['Zerind', 75], ['Timisoara', 118]],
+    'Arad':             [['Zerind', 75], ['Timisoara', 118], ['Sibiu', 140]],
 	'Bucharest':        [['Urziceni', 85], ['Giurgiu', 90], ['Pitesti', 101], ['Fagaras', 211]],
 	'Craiova':          [['Dobreta', 120], ['Pitesti', 138], ['Rimnicu Vilcea', 146]],
 	'Dobreta':          [['Mehadia', 75], ['Craiova', 120]],
@@ -24,27 +23,72 @@ city_paths = {
 	'Vaslui':           [['Iasi', 92], ['Urziceni', 142]],
 	'Zerind':           [['Oradea', 71], ['Arad', 75]]
 }
+line_to_Bucharest={
+    'Arad':             418,
+	'Bucharest':        0,
+	'Craiova':          239,
+	'Dobreta':          359,
+	'Eforie':           269,
+	'Fagaras':          211,
+	'Giurgiu':          90,
+	'Hirsova':          183,
+	'Iasi':             319,
+	'Lugoj':            504,
+	'Mehadia':          434,
+	'Neamt':            406,
+	'Oradea':           429,
+	'Pitesti':          101,
+	'Rimnicu Vilcea':   198,
+	'Sibiu':            278,
+	'Timisoara':        536,
+	'Urziceni':         85,
+	'Vaslui':           227,
+	'Zerind':           493
+}
 
-def bfs(root_city,distance_traveled,city_history):
-    city_history.append(root_city)
-    if DESTINATION in city_history: #check all cities traveled to
-        return (distance_traveled,city_history);
-    paths=[]
-    for i in range(len(city_paths[root_city])): #add all adjacent cities to paths
-        paths.append(city_paths[root_city][i])
-        if city_paths[root_city][i][0]==DESTINATION: #checks paths for Destination and returns Destination path if found
-            return bfs(DESTINATION,distance_traveled+city_paths[root_city][i][1],city_history)
+class Traverse:
+
+    distance_traveled=0
+    city_history=[]
+
+    def __init__(self,city) -> None:
+        self.city_history.append(city)
     
-    
+    #fix distance traveled
+    def aStar(self) -> tuple:
+        """
+        Recursively calls itself after finding the lowest cost path
+        at each city.\n
+        Returns a Traverse object
+        """
+        if self.city_history[-1]==DESTINATION: return self;
+        paths=[]
+        costs=[]
+        for i in range(len(city_paths[self.city_history[-1]])):
+            if city_paths[self.city_history[-1]][i][0] not in self.city_history:    #checks that city in city_paths has not been visited
+                paths.append(city_paths[self.city_history[-1]][i])
+        if not paths: return self,False;
+        for path in paths:#path=[city,distance]
+            costs.append(self.calculate_cost(path))
+        lowest_cost_path=paths[costs.index(min(costs))]
+        nextCity=Traverse(lowest_cost_path[0])
+        self.distance_traveled+=lowest_cost_path[1]
+        print(self.distance_traveled)
+        return nextCity.aStar()
+
+    def calculate_cost(self,path) -> int:
+        """f(n)=g(n)+h(n)"""
+        return (self.distance_traveled+path[1])+line_to_Bucharest[path[0]]
+
+def bfs():
+    #@Meike
     return None
 
-def a_star(root_city,distance_traveled,city_history):
-    openList=[]
-    closedList=[]
-    
-    return 0;
+def dfs():
+    #@Meike
+    return None
 
-def find_shortest_route():
+def find_shortest_route(): #reorganize after commit with bfs and dfs
   print("Purpose of app here")
   print("Which city do you want to depart from?")
   #print list of cities, could be list of keys from city_paths
@@ -57,8 +101,11 @@ def find_shortest_route():
   #print algo + cities visited + distances + total distance
   #loop or quit
   return 0
-#find_shortest_route()
-paths=[]
-for i in range(len(city_paths['Arad'])):
-    paths.append(city_paths['Arad'][i])
-print(paths[0][0])
+
+
+#Test beyond this point
+tester=Traverse('Arad')
+tester.aStar()
+
+#print(tester.distance_traveled)
+print(tester.city_history)
